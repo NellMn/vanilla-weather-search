f379d549956dfc0ca08cba1437d22eff13fc0ca3
NellMn <118570134+NellMn@users.noreply.github.com>
2023-01-14 15:30:11 +0330

    Injecting HTML from the JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timesTamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2" id="eachForecast">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/04d@2x.png"
+          alt=""
+          width="44"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 4° </span>
+          <span class="weather-forecast-temperature-min"> 0° </span>
+        </div>
+      </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showResult(response) {
   let currentTemperature = document.querySelector("#current-temperature");
   let maxTemperature = document.querySelector("#max-temperature");
@@ -93,3 +122,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("Oregon");
+displayForecast();
